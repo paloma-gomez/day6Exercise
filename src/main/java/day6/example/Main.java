89694a703710b611b559570ce0036d7209380c7f
Paloma Gomez Abad <paloma.gomez@gruposantander.com>
2025-09-22 10:58:28 +0200

    changes from branch 2

--- src/main/java/day6/example/Main.java
+++ src/main/java/day6/example/Main.java
@@ -2,6 +2,8 @@ package day6.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World from Branch 2!");
+        int branch2 = 10000;
+        System.out.println("Branch 2 value: " + branch2);
     }
 }
