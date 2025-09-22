cf05d1ee78ffc5f7e5a097c8420e6eb86628faf4
Paloma Gomez Abad <paloma.gomez@gruposantander.com>
2025-09-22 10:25:31 +0200

    changes in feature branch

--- src/main/java/day6/example/Main.java
+++ src/main/java/day6/example/Main.java
@@ -2,6 +2,6 @@ package day6.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Opentechies!");
     }
 }
